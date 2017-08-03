5cf78e89a12168674969be2b598d70e70c373520
Akshay Dongaonkar <akshay.dongaonkar@phantom.us>
2017-08-03 16:23:19 -0700

    PAPP-2955 Update copyright start to 2016

--- FalconAPI_consts.py
+++ FalconAPI_consts.py
@@ -1,7 +1,7 @@
 # --
 # File: FalconAPI_consts.py
 #
-# Copyright (c) Phantom Cyber Corporation, 2014-2017
+# Copyright (c) Phantom Cyber Corporation, 2016-2017
 #
 # This unpublished material is proprietary to Phantom Cyber.
 # All rights reserved. The methods and
